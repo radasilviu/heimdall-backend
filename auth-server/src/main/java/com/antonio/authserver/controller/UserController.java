ab924cb22f903815468b5e0a278e4cca8914d9a2
Vinter Antonio <tonyivinter@gmail.com>
2020-09-17 10:28:25 +0300

    added AddRole to user

--- auth-server/src/main/java/com/antonio/authserver/controller/UserController.java
+++ auth-server/src/main/java/com/antonio/authserver/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.antonio.authserver.controller;
 
 import com.antonio.authserver.entity.AppUser;
+import com.antonio.authserver.entity.Role;
 import com.antonio.authserver.model.ResponseMessage;
 import com.antonio.authserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,11 @@ public class UserController {
         final ResponseMessage responseMessage = new ResponseMessage("User successfully saved");
 
         return ResponseEntity.ok().body(responseMessage);
+    }
+
+    @PostMapping("/{id}/addRole")
+    public void addRoleToUser(@PathVariable Long user,@RequestBody Role role){
+        userService.addRole((long) 1,role);
     }
 
 
