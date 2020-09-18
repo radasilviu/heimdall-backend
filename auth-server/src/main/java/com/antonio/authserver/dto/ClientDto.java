b0c80a05ab3820e5fcf8ac9a8219ad89ccbfd217
Ioan-Octavian Patcas <octavianp555@gmail.com>
2020-09-18 13:04:48 +0300

    Added DTOS

---
+++ auth-server/src/main/java/com/antonio/authserver/dto/ClientDto.java
@@ -0,0 +1,17 @@
+package com.antonio.authserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientDto {
+
+    private String clientName;
+
+    private String clientSecret;
+}
