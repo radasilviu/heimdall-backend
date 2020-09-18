14f6824a54ae4f5040a85fc97c5e02cbf9a67976
Silviu Rada <silviu.rada@gmail.com>
2020-09-18 08:23:37 +0300

    example of handle exceptions

--- auth-server/src/main/java/com/antonio/authserver/service/UserService.java
+++ auth-server/src/main/java/com/antonio/authserver/service/UserService.java
@@ -2,11 +2,15 @@ package com.antonio.authserver.service;
 
 import com.antonio.authserver.entity.AppUser;
 import com.antonio.authserver.entity.Role;
+import com.antonio.authserver.model.exceptions.AuthorizationServerError;
 import com.antonio.authserver.repository.AppUserRepository;
 import com.antonio.authserver.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,18 +41,20 @@ public class UserService {
     }
 
 
-    public void addRole(Long userId, Role role) {
+    public ResponseEntity<?> addRole(Long userId, Role role) {
         if (!checkIfUserExist(userId)) {
-            throw new RuntimeException("User with id: " + userId + "doesn't exist");
+            return ResponseEntity.badRequest().body(new AuthorizationServerError(HttpStatus.BAD_REQUEST, LocalDateTime.now(), "User with id: " + userId + "doesn't exist"));
         }
 
         if (!checkIfRoleExist(role.getId())) {
-            throw new RuntimeException("Role  with id: " + role.getId() + "can not be added to user, it need to be saved firstly");
+            return ResponseEntity.badRequest().body(new AuthorizationServerError(HttpStatus.BAD_REQUEST, LocalDateTime.now(), "Role  with id: " + role.getId() + "can not be added to user, it need to be saved firstly"));
         }
 
         AppUser user = appUserRepository.getOne(userId);
         user.getRoles().add(role);
         appUserRepository.save(user);
+
+        return ResponseEntity.ok().body(user);
     }
 
     public void removeRole(Long userId, Role role) {
@@ -63,6 +69,8 @@ public class UserService {
         AppUser user = appUserRepository.getOne(userId);
         user.getRoles().remove(role);
         appUserRepository.save(user);
+
+
     }
 
 
