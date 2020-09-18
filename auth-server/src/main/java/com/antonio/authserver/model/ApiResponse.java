6813ad4de0b5002c46f462977e60bf1b205a81d5
Ioan-Octavian Patcas <octavianp555@gmail.com>
2020-09-18 11:34:07 +0300

    Added exceptions

---
+++ auth-server/src/main/java/com/antonio/authserver/model/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.antonio.authserver.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse {
+    private int status;
+    private String message;
+    private Object result;
+
+    public ApiResponse(int status, String message, Object result) {
+        this.status = status;
+        this.message = message;
+        this.result = result;
+    }
+
+    public ApiResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiResponse{" +
+                "statusCode=" + status +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
