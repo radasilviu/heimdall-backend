a710966bb44917b5e201fdf16ab022f7f5a6b1f9
Ioan-Octavian Patcas <octavianp555@gmail.com>
2020-09-21 12:29:28 +0300

    Refactored role service exception handling

--- auth-server/src/main/java/com/antonio/authserver/service/RoleService.java
+++ auth-server/src/main/java/com/antonio/authserver/service/RoleService.java
@@ -3,6 +3,8 @@ package com.antonio.authserver.service;
 import com.antonio.authserver.dto.RoleDto;
 import com.antonio.authserver.entity.Role;
 import com.antonio.authserver.mapper.RoleMapper;
+import com.antonio.authserver.model.exceptions.controllerexceptions.RoleAlreadyExists;
+import com.antonio.authserver.model.exceptions.controllerexceptions.RoleNotFound;
 import com.antonio.authserver.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,16 +26,21 @@ public class RoleService {
     }
 
 
-    public void saveRole(RoleDto role) {
-        roleRepository.save(roleMapper.toRoleDao(role));
+    public void saveRole(RoleDto role) throws RoleAlreadyExists {
+        if (roleRepository.findByName(role.getName()).isPresent())
+            throw new RoleAlreadyExists(role.getName());
+        else {
+            roleRepository.save(roleMapper.toRoleDao(role));
+        }
     }
 
     public List<RoleDto> getAllRoles() {
         return roleMapper.toRoleDtoList(roleRepository.findAll());
     }
 
-    public void deleteRoleByName(String name) {
-        
-        roleRepository.delete(roleRepository.findByName(name).get());
+    public void deleteRoleByName(String name) throws RoleNotFound {
+
+        Role role = roleRepository.findByName(name).orElseThrow(() -> new RoleNotFound(name));
+        roleRepository.delete(role);
     }
 }
