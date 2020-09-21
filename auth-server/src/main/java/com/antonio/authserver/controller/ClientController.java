588e67fd6832e26e3a9496a33348c3ad5aeab25c
Silviu Rada <silviu.rada@gmail.com>
2020-09-21 09:07:10 +0300

    resolve client controller errors after merge

--- auth-server/src/main/java/com/antonio/authserver/controller/ClientController.java
+++ auth-server/src/main/java/com/antonio/authserver/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.antonio.authserver.controller;
 
+import com.antonio.authserver.dto.ClientDto;
 import com.antonio.authserver.entity.Client;
 import com.antonio.authserver.model.ResponseMessage;
 import com.antonio.authserver.service.ClientService;
@@ -23,31 +24,31 @@ public class ClientController {
     }
 
     @GetMapping
-    public List<Client> getAllClients() {
-        List<Client> clients = clientService.getAllClients();
+    public List<ClientDto> getAllClients() {
+        List<ClientDto> clients = clientService.getAllClients();
 
 //        return ResponseEntity.ok().body(clients);
         return clients;
     }
 
-    @GetMapping("/{clientId}")
-    public ResponseEntity<Client> getClientById(@PathVariable Long clientId) {
-        Client client = clientService.getClientById(clientId);
+    @GetMapping("/{clientName}")
+    public ResponseEntity<ClientDto> getClientByName(@PathVariable String clientName) {
+        ClientDto client = clientService.getClientByName(clientName);
 
         return ResponseEntity.ok().body(client);
     }
 
     @PostMapping
-    public ResponseEntity<ResponseMessage> saveClient(@RequestBody Client client) {
+    public ResponseEntity<ResponseMessage> saveClient(@RequestBody ClientDto client) {
 
         clientService.saveClient(client);
         final ResponseMessage responseMessage = new ResponseMessage("Client successfully saved");
         return ResponseEntity.ok().body(responseMessage);
     }
 
-    @DeleteMapping("/{clientId}")
-    public ResponseEntity<ResponseMessage> deleteClient(@PathVariable Long clientId) {
-        clientService.deleteClientById(clientId);
+    @DeleteMapping("/{clientName}")
+    public ResponseEntity<ResponseMessage> deleteClient(@PathVariable String clientName) {
+        clientService.deleteClientByName(clientName);
         final ResponseMessage responseMessage = new ResponseMessage("Client successfully deleted");
         return ResponseEntity.ok().body(responseMessage);
     }
