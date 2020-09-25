abf7b0fd28475caf74519ba58cd53284300a724a
Ioan-Octavian Patcas <octavianp555@gmail.com>
2020-09-25 11:35:25 +0300

    DB queries

--- auth-server/src/main/java/com/antonio/authserver/repository/AppUserRepository.java
+++ auth-server/src/main/java/com/antonio/authserver/repository/AppUserRepository.java
@@ -1,26 +1,32 @@
 package com.antonio.authserver.repository;
-
-import com.antonio.authserver.entity.AppUser;
-import com.antonio.authserver.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.antonio.authserver.entity.AppUser;
+import com.antonio.authserver.entity.Role;
+
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
-    Optional<AppUser> findByUsername(String name);
+	Optional<AppUser> findByUsername(String name);
 
-    Optional<AppUser> findByUsernameAndPassword(String username, String password);
+	Optional<AppUser> findByUsernameAndPassword(String username, String password);
 
-    Optional<AppUser> findByCode(String code);
+	Optional<AppUser> findByCode(String code);
 
-    List<AppUser> findAllByRolesIn(Set<Role> roles);
+	List<AppUser> findAllByRolesIn(Set<Role> roles);
 
-    void deleteByUsername(String username);
+	void deleteByUsername(String username);
 
-    Optional<AppUser> findByToken(String token);
+	Optional<AppUser> findByToken(String token);
+
+	@Query("SELECT u FROM AppUser u WHERE  u.emailCode =?1")
+	Optional<AppUser> findByEmailCode(String emailCode);
+
+	@Query("UPDATE AppUser u SET u.isActivated=true WHERE u.username= ?1")
+	void activate(String username);
 }
