ecbc3e6a764fa2ce0d4c75f7bdc9d332f314d032
Silviu Rada <silviu.rada@gmail.com>
2020-09-28 14:50:08 +0300

    remove post method from security

--- auth-server/src/main/java/com/antonio/authserver/configuration/WebSecurityConfig.java
+++ auth-server/src/main/java/com/antonio/authserver/configuration/WebSecurityConfig.java
@@ -2,8 +2,6 @@ package com.antonio.authserver.configuration;
 
 import com.antonio.authserver.configuration.auth_providers.UsernameAndPasswordAuthProvider;
 import com.antonio.authserver.configuration.filters.JwtTokenVerifier;
-import com.antonio.authserver.dto.AppUserDto;
-import com.antonio.authserver.model.ResponseMessage;
 import com.antonio.authserver.model.exceptions.RestAccessDeniedHandler;
 import com.antonio.authserver.model.exceptions.RestAuthenticationEntryPoint;
 import com.antonio.authserver.service.JwtService;
@@ -11,18 +9,14 @@ import com.antonio.authserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Configuration
 @EnableWebSecurity
@@ -63,15 +57,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(usernameAndPasswordAuthProvider);
     }
-
-
-    @PostMapping
-    public ResponseEntity<?> saveUser(@RequestBody final AppUserDto user) {
-        userService.create(user);
-        final ResponseMessage responseMessage = new ResponseMessage("User successfully saved");
-        return ResponseEntity.ok().body(responseMessage);
-    }
-
 
     @Override
     @Bean
