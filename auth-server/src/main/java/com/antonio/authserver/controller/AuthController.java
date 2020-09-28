39596f3fbc46b34349fb65628b5d073fcd1f618e
Ioan-Octavian Patcas <octavianp555@gmail.com>
2020-09-28 17:19:25 +0300

    fix

--- auth-server/src/main/java/com/antonio/authserver/controller/AuthController.java
+++ auth-server/src/main/java/com/antonio/authserver/controller/AuthController.java
@@ -29,7 +29,6 @@ public class AuthController {
 	@Autowired
 	private EmailService emailService;
 
-	@CrossOrigin("http://localhost:4201")
 	@PostMapping(path = "/client-login")
 	public ResponseEntity<?> clientLogin(@RequestBody ClientLoginRequest loginRequest) {
 		Code code = authService.getCode(loginRequest);
@@ -40,11 +39,11 @@ public class AuthController {
 		return ResponseEntity.unprocessableEntity().body(responseMessage);
 	}
 
-    @PostMapping(path = "/token")
-    public ResponseEntity<?> getToken(@RequestBody LoginCredential loginCredential) {
-        JwtObject jwtObject = authService.login(loginCredential);
-        return ResponseEntity.ok().body(jwtObject);
-    }
+	@PostMapping(path = "/token")
+	public ResponseEntity<?> getToken(@RequestBody LoginCredential loginCredential) {
+		JwtObject jwtObject = authService.login(loginCredential);
+		return ResponseEntity.ok().body(jwtObject);
+	}
 
 	@PutMapping(path = "/refreshToken")
 	public ResponseEntity<?> getNewTokenByRefreshToken(@RequestBody JwtObject refreshToken) {
@@ -52,21 +51,19 @@ public class AuthController {
 		return ResponseEntity.ok().body(jwtObject);
 	}
 
-    @PostMapping(path = "/token/delete")
-    public ResponseEntity<?> deleteToken(@RequestBody JwtObject jwtObject) {
-        authService.logout(jwtObject);
-        final ResponseMessage responseMessage = new ResponseMessage("User loogged out");
-        return ResponseEntity.ok().body(responseMessage);
-    }
+	@PostMapping(path = "/token/delete")
+	public ResponseEntity<?> deleteToken(@RequestBody JwtObject jwtObject) {
+		authService.logout(jwtObject);
+		final ResponseMessage responseMessage = new ResponseMessage("User loogged out");
+		return ResponseEntity.ok().body(responseMessage);
+	}
 
-	@CrossOrigin("http://localhost:8080")
 	@GetMapping(path = "/access")
 	public ResponseEntity<?> verifyToken() {
 		final ResponseMessage responseMessage = new ResponseMessage("Valid Token");
 		return ResponseEntity.ok().body(responseMessage);
 	}
 
-	@CrossOrigin("http://localhost:4200")
 	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@RequestBody AppUserDto user, HttpServletRequest httpServletRequest)
 			throws TemplateException, IOException, MessagingException {
--- auth-server/src/main/java/com/antonio/authserver/controller/EmailController.java
+++ auth-server/src/main/java/com/antonio/authserver/controller/EmailController.java
@@ -2,7 +2,6 @@ package com.antonio.authserver.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.antonio.authserver.dto.AppUserDto;
@@ -15,7 +14,7 @@ public class EmailController {
 	public EmailController(EmailService emailService) {
 		this.emailService = emailService;
 	}
-	@CrossOrigin("http://localhost:8081")
+
 	@GetMapping("/activate")
 	public String activateAccount(@Param("emailCode") String emailCode) {
 		AppUserDto appUserDto = emailService.verifyAndActivateEmailCode(emailCode);
