1c27faa9fc26382dfdd28df4afc5c35a9111ce52
Vinter Antonio <tonyivinter@gmail.com>
2020-10-23 14:21:40 +0300

    del extra data

--- auth-server/src/main/java/com/antonio/authserver/service/ClientService.java
+++ auth-server/src/main/java/com/antonio/authserver/service/ClientService.java
@@ -11,15 +11,9 @@ import com.antonio.authserver.repository.ClientRepository;
 import com.antonio.authserver.repository.RealmRepository;
 import com.antonio.authserver.utils.SecurityConstants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
