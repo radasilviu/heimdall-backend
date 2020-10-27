bc93750043945b14f129f8715a217b7a333742b1
sebastian-sirb <sebastian.sirb@fortech.ro>
2020-10-27 16:00:15 +0200

    Minor change in PrivilegeController: removed duplicate method

--- auth-server/src/main/java/com/antonio/authserver/controller/PrivilegeController.java
+++ auth-server/src/main/java/com/antonio/authserver/controller/PrivilegeController.java
@@ -25,11 +25,6 @@ public class PrivilegeController {
         return ResponseEntity.ok().body(privilegeService.getAllPrivileges());
     }
 
-    @PostMapping("/{resourceName}/privileges")
-    public ResponseEntity<List<PrivilegeDto>> getPrivilegesForRole(@PathVariable String resourceName, @RequestBody RoleDto roleDto){
-        return ResponseEntity.ok().body(privilegeService.getPrivilegesForResource(resourceName,roleDto));
-    }
-
     @PostMapping("/{resourceName}")
     public ResponseEntity<List<PrivilegeDto>> getPrivilegesForResource(@PathVariable String resourceName, @RequestBody RoleDto roleDto){
         return ResponseEntity.ok().body(privilegeService.getPrivilegesForResource(resourceName, roleDto));
