4dabe203463ddd4f617a3e236349c8134df8c2b9
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-10-27 10:26:05 +0200

    Various small changes in PrivilegeController: HTTP method changes, code clean-up

--- auth-server/src/main/java/com/antonio/authserver/controller/PrivilegeController.java
+++ auth-server/src/main/java/com/antonio/authserver/controller/PrivilegeController.java
@@ -1,4 +1,5 @@
 package com.antonio.authserver.controller;
+
 import com.antonio.authserver.dto.PrivilegeDto;
 import com.antonio.authserver.dto.RoleDto;
 import com.antonio.authserver.model.ResponseMessage;
@@ -6,9 +7,8 @@ import com.antonio.authserver.service.PrivilegeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.transaction.Transactional;
 import java.util.List;
+
 @RestController
 @RequestMapping("api/privilege")
 public class PrivilegeController {
@@ -19,25 +19,28 @@ public class PrivilegeController {
     public PrivilegeController(PrivilegeService privilegeService) {
         this.privilegeService = privilegeService;
     }
+
     @GetMapping
     public ResponseEntity<List<PrivilegeDto>> getAllPrivileges(){
         return ResponseEntity.ok().body(privilegeService.getAllPrivileges());
     }
-    @GetMapping("/{resourceName}")
-    public ResponseEntity<List<PrivilegeDto>> getPrivilegesForRole(@PathVariable String resourceName,@RequestBody RoleDto roleDto){
+
+    @PostMapping("/{resourceName}")
+    public ResponseEntity<List<PrivilegeDto>> getPrivilegesForRole(@PathVariable String resourceName, @RequestBody RoleDto roleDto){
         return ResponseEntity.ok().body(privilegeService.getPrivilegesForResource(resourceName,roleDto));
     }
+
     @PutMapping("/{privilegeName}/{resourceName}/add")
-    public ResponseEntity<ResponseMessage> addPrivilegeToRole(@PathVariable String privilegeName, @PathVariable String resourceName,@RequestBody RoleDto role){
-        privilegeService.addPrivilegeToResourceForRole(privilegeName,resourceName,role);
+    public ResponseEntity<ResponseMessage> addPrivilegeToRole(@PathVariable String privilegeName, @PathVariable String resourceName, @RequestBody RoleDto role){
+        privilegeService.addPrivilegeToResourceForRole(privilegeName, resourceName, role);
         ResponseMessage responseMessage = new ResponseMessage("Privilege added to role successfully!");
         return ResponseEntity.ok().body(responseMessage);
     }
+
     @PutMapping("/{privilegeName}/{resourceName}/remove")
-    public ResponseEntity<ResponseMessage> removePrivilegeFromRole(@PathVariable String privilegeName, @PathVariable String resourceName,@RequestBody RoleDto role){
-        privilegeService.removePrivilegeFromRole(privilegeName,resourceName,role);
+    public ResponseEntity<ResponseMessage> removePrivilegeFromRole(@PathVariable String privilegeName, @PathVariable String resourceName, @RequestBody RoleDto role){
+        privilegeService.removePrivilegeFromRole(privilegeName, resourceName, role);
         ResponseMessage responseMessage = new ResponseMessage("Privilege removed from role successfully!");
         return ResponseEntity.ok().body(responseMessage);
     }
-
 }
