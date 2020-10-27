0e11d5ab3b20a1a47638e35e0ad456cc9acd959b
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-10-27 10:12:27 +0200

    Various small changes in ResourceController: path changes, method param changes, HTTP method changes

--- auth-server/src/main/java/com/antonio/authserver/controller/ResourceController.java
+++ auth-server/src/main/java/com/antonio/authserver/controller/ResourceController.java
@@ -28,41 +28,50 @@ public class ResourceController {
     public ResponseEntity<List<ResourceDto>> getAllResources(){
         return ResponseEntity.ok().body(resourceService.getAllResources());
     }
-    @GetMapping
+
+    @PostMapping("/getResources")
     public ResponseEntity<List<ResourceDto>> getAllResourcesForRoleDto(@RequestBody RoleDto roleDto){
         return ResponseEntity.ok().body(resourceService.getAllResourcesForRole(roleDto));
     }
-    @GetMapping("/{resourceName}")
+
+    @PostMapping("/{resourceName}")
     public ResponseEntity<List<PrivilegeDto>> getPrivilegesForResource(@PathVariable String resourceName, @RequestBody RoleDto roleDto){
-        return ResponseEntity.ok().body(privilegeService.getPrivilegesForResource(resourceName,roleDto));
+        return ResponseEntity.ok().body(privilegeService.getPrivilegesForResource(resourceName, roleDto));
     }
+
     @PostMapping
     public ResponseEntity<ResponseMessage> addResourceToDatabase(@RequestBody ResourceDto resourceDto){
         resourceService.createResourceForAllRoles(resourceDto);
         ResponseMessage responseMessage = new ResponseMessage("Resource created successfully!");
         return ResponseEntity.ok().body(responseMessage);
     }
-    @PutMapping("/{realmName}/{roleName}/{resourceName}/add")
-    public ResponseEntity<ResponseMessage> addResourceToRoleDto(@PathVariable String realmName,@PathVariable String roleName, @PathVariable String resourceName){
-        resourceService.addResourceToRole(realmName,roleName,resourceName);
-        ResponseMessage responseMessage = new ResponseMessage("Resource added successfully!");
-        return ResponseEntity.ok().body(responseMessage);
-    }
-    @PutMapping("/{realmName}/{roleName}/{resourceName}/remove")
-    public ResponseEntity<ResponseMessage> removeResourceFromRoleDto(@PathVariable String realmName,@PathVariable String roleName, @PathVariable String resourceName){
-        resourceService.removeResourceFromRole(realmName,roleName,resourceName);
-        ResponseMessage responseMessage = new ResponseMessage("Resource removed successfully!");
-        return ResponseEntity.ok().body(responseMessage);
-    }
+
+//    // This could just as well be a GET, we don't use the body
+//    @PutMapping("/{resourceName}/add")
+//    public ResponseEntity<ResponseMessage> addResourceToRoleDto(@PathVariable String resourceName, @RequestBody Rol){
+//        resourceService.addResourceToRole(realmName,roleName,resourceName);
+//        ResponseMessage responseMessage = new ResponseMessage("Resource added successfully!");
+//        return ResponseEntity.ok().body(responseMessage);
+//    }
+//
+//    // This could just as well be a GET, we don't use the body; See removeResourceFromRole
+//    @PutMapping("/{realmName}/{roleName}/{resourceName}/remove")
+//    public ResponseEntity<ResponseMessage> removeResourceFromRoleDto(@PathVariable String realmName,@PathVariable String roleName, @PathVariable String resourceName){
+//        resourceService.removeResourceFromRole(realmName,roleName,resourceName);
+//        ResponseMessage responseMessage = new ResponseMessage("Resource removed successfully!");
+//        return ResponseEntity.ok().body(responseMessage);
+//    }
+
     @DeleteMapping("/{resourceName}/removeAll")
     public ResponseEntity<ResponseMessage> removeResourceFromDatabase(@PathVariable String resourceName){
         resourceService.deleteResourceForAllRoles(resourceName);
         ResponseMessage responseMessage = new ResponseMessage("Resource deleted successfully!");
         return ResponseEntity.ok().body(responseMessage);
     }
+
     @DeleteMapping("/{resourceName}")
-    public ResponseEntity<ResponseMessage> removeResourceFromUser(@PathVariable String resourceName,@RequestBody RoleDto roleDto){
-        resourceService.deleteResourceForRole(resourceName,roleDto);
+    public ResponseEntity<ResponseMessage> removeResourceFromRole(@PathVariable String resourceName, @RequestBody RoleDto roleDto){
+        resourceService.deleteResourceForRole(resourceName, roleDto);
         ResponseMessage responseMessage = new ResponseMessage("Resource deleted successfully for role!");
         return ResponseEntity.ok().body(responseMessage);
     }
