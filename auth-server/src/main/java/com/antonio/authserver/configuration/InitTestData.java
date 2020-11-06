5c6e73a810dd3b0218fbfb370f1370216c7c6916
sebastian-sirb <sebastian.sirb@fortech.ro>
2020-11-06 13:58:37 +0200

    Improved readability in InitTestData and modified users' initial email addresses

--- auth-server/src/main/java/com/antonio/authserver/configuration/InitTestData.java
+++ auth-server/src/main/java/com/antonio/authserver/configuration/InitTestData.java
@@ -10,9 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Component
 @Log4j2
@@ -43,34 +41,53 @@ public class InitTestData implements ApplicationListener<ApplicationContextEvent
     public void onApplicationEvent(ApplicationContextEvent applicationContextEvent) {
         // for demo purpose
         if (roleRepository.findAll().size() == 0) {
-
-            List<Realm> realms = new ArrayList<>();
-            for (int i = 0; i < 5; i++) {
-                Realm realm = new Realm();
-                realm.setName("master" + i);
-                realm.setDisplayName("Master " + i);
-                realm.setEnabled(true);
-                realms.add(realm);
-            }
-            realms = realmRepository.saveAll(realms);
-
-            final IdentityProvider google = new IdentityProvider("GOOGLE");
-            final IdentityProvider usernameAndPassword = new IdentityProvider("USERNAME_AND_PASSWORD");
-            List<IdentityProvider> identityProviders = Arrays.asList(google, usernameAndPassword);
-            identityProviderRepository.saveAll(identityProviders);
-
-            List<Role> roleList = Arrays.asList(new Role("ROLE_ADMIN",realms.get(0)), new Role("ROLE_USER",realms.get(0)));
-            roleRepository.saveAll(roleList);
-
-            AppUser user = new AppUser("test", passwordEncoder.encode("test"), roleRepository.findAllByName("ROLE_USER"), "test@gmail.com", true, null, realms.get(0), usernameAndPassword);
-            AppUser admin = new AppUser("admin", passwordEncoder.encode("admin"), roleRepository.findAllByName("ROLE_ADMIN"), "test@gmail.com", true, null, realms.get(0), usernameAndPassword);
-            appUserRepository.save(user);
-            appUserRepository.save(admin);
-
-            Client client = new Client("myClient", "clientPass", realms.get(0));
-            clientRepository.save(client);
-
+            final List<Realm> realms = createRealms();
+            final Map<String, IdentityProvider> identityProviders = createIdentityProviders();
+            createRoles(realms);
+            createUsers(realms, identityProviders);
+            createClients(realms);
             groupRepository.save(new UserGroup("Group1", new ArrayList<>(), realms.get(1)));
         }
+    }
+
+    private Map<String, IdentityProvider> createIdentityProviders() {
+        Map<String, IdentityProvider> identityProviders = new HashMap<>();
+        final IdentityProvider google = new IdentityProvider("GOOGLE");
+        final IdentityProvider usernameAndPassword = new IdentityProvider("USERNAME_AND_PASSWORD");
+        identityProviders.put("google", google);
+        identityProviders.put("username_password", usernameAndPassword);
+        identityProviderRepository.saveAll(identityProviders.values());
+        return identityProviders;
+    }
+
+    private List<Realm> createRealms() {
+        List<Realm> realms = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Realm realm = new Realm();
+            realm.setName("master" + i);
+            realm.setDisplayName("Master " + i);
+            realm.setEnabled(true);
+            realms.add(realm);
+        }
+        realms = realmRepository.saveAll(realms);
+        return realms;
+    }
+
+    private void createRoles(List<Realm> realms) {
+        List<Role> roleList = Arrays.asList(new Role("ROLE_ADMIN", realms.get(0)), new Role("ROLE_USER", realms.get(0)));
+        roleRepository.saveAll(roleList);
+    }
+
+    private void createClients(List<Realm> realms) {
+        Client client = new Client("myClient", "clientPass", realms.get(0));
+        clientRepository.save(client);
+    }
+
+    private void createUsers(List<Realm> realms, Map<String, IdentityProvider> identityProviders) {
+        IdentityProvider usernameAndPassword = identityProviders.get("username_password");
+        AppUser user = new AppUser("test", passwordEncoder.encode("test"), roleRepository.findAllByName("ROLE_USER"), "admin@gmail.com", true, null, realms.get(0), usernameAndPassword);
+        AppUser admin = new AppUser("admin", passwordEncoder.encode("admin"), roleRepository.findAllByName("ROLE_ADMIN"), "test@gmail.com", true, null, realms.get(0), usernameAndPassword);
+        appUserRepository.save(user);
+        appUserRepository.save(admin);
     }
 }
